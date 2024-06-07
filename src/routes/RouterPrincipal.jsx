ac7a56c4436bb3e8c9c8d2697490f34ed7043db8
Gerardo <jatipg@gmail.com>
2024-06-06 23:34:29 -0300

    agregando navlink

--- src/routes/RouterPrincipal.jsx
+++ src/routes/RouterPrincipal.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Routes, NavLink, BrowserRouter, Route } from 'react-router-dom'
+import PacienteElegido from '../components/PacienteElegido'
 
 const RouterPrincipal = () => {
   return (
 
     //Cuando creo esta estructura, es lo mismo que trabajar con un componente, solo que utilizando el router dom
     <BrowserRouter>
+        <nav className='navPrincipal'>
+          <ul>
+            <li><NavLink to="">Pacientes</NavLink></li>
+            <li><NavLink to="">Cerrar Sesión</NavLink></li>
+          </ul>
+        </nav>
         <Routes>
-            <Route path='/paciente' element={""}/>
+            <Route path='/pacientes' element={<PacienteElegido />}/>
         </Routes>
     </BrowserRouter>
   )
