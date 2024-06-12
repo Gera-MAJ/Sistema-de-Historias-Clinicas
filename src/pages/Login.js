70296764e43596c637cceb09d577eab488b0fa6e
Leticia Araoz <araoz.leticia@gmail.com>
2024-06-11 22:06:40 -0300

    Loguin agregado

---
+++ src/pages/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (username === 'admin' && password === '1234') {
+        alert('Ingresaste exitosamente!');
+      } else {
+        setErrorMessage('Usuario y/o Contraseña inválidos');
+      }
+    };
+  
+    return (
+      <div className="login-container">
+        <form className="login-form" onSubmit={handleSubmit}>
+          <h2>¡Bienvenida!</h2>
+          <div>
+            <label htmlFor="username">Usuario:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Contraseña:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+          <button type="submit">Ingresar</button>
+        </form>
+      </div>
+    );
+  };
+  
+  export default Login;
