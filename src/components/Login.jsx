bdcf524f96e0e4841dea9ee61e14836158408a21
Gerardo <jatipg@gmail.com>
2024-08-08 22:33:08 -0300

    agregando nuevas cosas

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -55,7 +55,7 @@ const Login = () => {
       <div className="login-container">
         <h2>Sistema de Historias Clínicas</h2>
         <form className="login-form" onSubmit={handleSubmit}>
-          <img src='/Ingreso de usuario sin fondo 01.png' alt="foto-login" />
+          <img src='/Ingreso_de_usuario_sin_fondo_01.png' alt="foto-login" />
           
           <section className='usuario'>
           
