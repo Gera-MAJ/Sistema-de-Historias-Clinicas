23de55db83945bce094c11721eda2254ccddd680
Gerardo <jatipg@gmail.com>
2024-08-19 20:20:19 -0300

    cambios en la parte de nuevo paciente

--- src/components/NuevoPaciente.jsx
+++ src/components/NuevoPaciente.jsx
@@ -80,52 +80,61 @@ const NuevoPaciente = () => {
     <div className="nuevo-paciente-container">
       <h2>Nuevo Paciente</h2>
       <form onSubmit={handleSubmit} className="nuevo-paciente-form">
-        <label>Apellido/s:
+        <section className="derecha">
+        <label>Apellido/s
           <input type="text" name="Apellidos" value={formData.Apellidos} onChange={handleChange} required />
         </label>
-        <label>Nombre/s:
+        <label>Nombre/s
           <input type="text" name="Nombres" value={formData.Nombres} onChange={handleChange} required />
         </label>
-        <label>Edad:
+        <label>Edad
           <input type="number" name="Edad" value={formData.Edad} onChange={handleChange} required />
         </label>
-        <label>Fecha de Nacimiento:
+        <label>Fecha de Nacimiento
           <input type="date" name="Fecha_de_Nacimiento" value={formData.Fecha_de_Nacimiento} onChange={handleChange} required />
         </label>
-        <label>DNI:
+        <label>DNI
           <input type="number" name="DNI" value={formData.DNI} onChange={handleChange} required />
         </label>
-        <label>Dirección:
+        <label>Dirección
           <input type="text" name="Domicilio" value={formData.Direccion} onChange={handleChange} required />
         </label>
-        <label>Localidad:
+        <label>Localidad
           <input type="text" name="Localidad" value={formData.Localidad} onChange={handleChange} required />
         </label>
-        <label>Teléfono:
+        <label>Teléfono
           <input type="number" name="Telefono" value={formData.Telefono} onChange={handleChange} required />
         </label>
-        <label>Ocupación:
+        </section>
+
+        <section className="izquierda">
+        
+        <label>Ocupación
           <input type="text" name="Ocupacion" value={formData.Ocupacion} onChange={handleChange} required />
         </label>
-        <label>Estado Civil:
+        <label>Estado Civil
           <input type="text" name="Estado_Civil" value={formData.Estado_Civil} onChange={handleChange} required />
         </label>
-        <label>Licencia:
+        <label>Licencia
           <input type="text" name="Licencia" value={formData.Licencia} onChange={handleChange} required />
         </label>
-        <label>Responsable:
+        <label>Responsable
           <input type="text" name="Responsable" value={formData.Responsable} onChange={handleChange} required />
         </label>
-        <label>Obra Social:
+        <label>Obra Social
           <input type="text" name="Obra_Social" value={formData.Obra_Social} onChange={handleChange} required />
         </label>
-        <label>N° de Afiliado:
+        <label>N° de Afiliado
           <input type="number" name="Num_de_Afiliado" value={formData.Num_de_Afiliado} onChange={handleChange} required />
         </label>
-        <label>Diagnóstico:
+        <label>Diagnóstico
           <input type="text" name="Diagnostico" value={formData.Diagnostico} onChange={handleChange} required />
         </label>
-        <button type="submit" className="submit-button">Registrar</button>
+        <label>
+          <button type="submit" className="submit-button">Registrar</button>
+        </label>
+        </section>
+        
       </form>
     </div>
   );
